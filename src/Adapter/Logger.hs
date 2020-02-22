025e88159ee594d88767019b08b5e73f5c73c47b
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2020-02-22 15:34:42 +0000

    quick logger file cleanup

--- src/Adapter/Logger.hs
+++ src/Adapter/Logger.hs
@@ -8,37 +8,32 @@ module Adapter.Logger where
 
 import           Control.Exception
 import           Katip
-
 import           ClassyPrelude
 import qualified Domain.Messages               as D
 import qualified Domain.User                   as D
 
 class Show a => Loggable a where
-    type R a
-    log' :: a -> R (IO ())
+    type F a
+    log' :: a -> F (IO ())
     show' :: a -> Text
 
 instance Show a => Loggable a where
-        type R a = KatipContextT IO ()
+        type F a = KatipContextT IO ()
         log' mess = $(logTM) ErrorS (showLS mess)
         show' = tshow
 
 instance Loggable D.Error where
-        type R D.Error = KatipContextT IO ()
+        type F D.Error = KatipContextT IO ()
         log' err = $(logTM) ErrorS (showLS err)
         show' = tshow
 
 instance Show a => Loggable ( D.Message a ) where
-        type R (D.Message a) = KatipContextT IO ()
+        type F (D.Message a) = KatipContextT IO ()
         log' (D.ErrorMsg   mess) = $(logTM) ErrorS (showLS $ show mess)
         log' (D.WarningMsg mess) = $(logTM) WarningS (showLS $ show mess)
         log' (D.InfoMsg    mess) = $(logTM) InfoS (showLS $ show mess)
         log' (D.DebugMsg   mess) = $(logTM) DebugS (showLS $ show mess)
-        show' = tshow 
-
-
-logsFormatting :: (Loggable a) => [a] -> KatipContextT IO ()
-logsFormatting = mapM_ log'
+        show' = tshow
 
 log :: (MonadIO m, Loggable a) => [a] -> m ()
 log elemsToLog = do
@@ -50,4 +45,4 @@ log elemsToLog = do
         liftIO
                 $ Control.Exception.bracket mkLogEnv closeScribes
                 $ \le -> runKatipContextT le () mempty
-                          $ logsFormatting elemsToLog
+                          $ mapM_ log' elemsToLog
