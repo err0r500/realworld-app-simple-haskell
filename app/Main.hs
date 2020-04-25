c592af826a57c56aae3695a5d7e6dae64b678fd6
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2020-04-25 15:08:02 +0000

    move Lib content to Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,62 @@
 module Main where
 
 import           RIO
-import           Lib
+import           System.IO
+
+import qualified Network.Wai.Handler.Warp      as Warp
+
+import qualified Config.Config                 as Config
+import qualified Adapter.EmailChecker          as EmailChecker
+import qualified Adapter.Http.Router           as Router
+import qualified Adapter.InMemory.UserRepo     as UserRepo
+import qualified Adapter.Fake.Hasher           as Hasher
+import qualified Adapter.Logger                as Logger
+import qualified Adapter.UUIDGen               as UUIDGen
+
+import qualified Usecase.Interactor            as UC
+import qualified Usecase.LogicHandler          as UC
+import qualified Usecase.UserRegistration      as UC
+import qualified Usecase.UserLogin             as UC
 
 main :: IO ()
-main = start
+main = do
+  putStrLn "== Haskel Clean Architecture =="
+  state  <- freshState
+  router <- Router.start (logicHandler interactor) $ run state
+  port   <- Config.getIntFromEnv "PORT" 3000
+  putStrLn $ "starting server on port: " ++ show port
+  Warp.run port router
+
+type State = TVar UserRepo.Store
+newtype App a = App (RIO State a) deriving (Applicative, Functor, Monad, MonadReader State, MonadIO)
+
+run :: State -> App a -> IO a
+run state (App app) = runRIO state app
+
+interactor :: UC.Interactor App
+interactor = UC.Interactor { UC.userRepo_         = userRepo
+                           , UC.checkEmailFormat_ = EmailChecker.checkEmailFormat
+                           , UC.genUUID_          = UUIDGen.genUUIDv4
+                           , UC.hashText_         = Hasher.hashText
+                           }
+ where
+  userRepo = UC.UserRepo UserRepo.getUserByID
+                         UserRepo.getUserByEmail
+                         UserRepo.getUserByName
+                         UserRepo.getUserByEmailAndHashedPassword
+
+logicHandler :: UC.Interactor App -> UC.LogicHandler App
+logicHandler i = UC.LogicHandler
+  (UC.register (UC.genUUID_ i)
+               (UC.checkEmailFormat_ i)
+               (UC.getUserByEmail_ $ UC.userRepo_ i)
+               (UC.getUserByName_ $ UC.userRepo_ i)
+  )
+  (UC.login (UC.hashText_ i) (UC.getUserByEmailAndHashedPassword_ $ UC.userRepo_ i))
+
+
+freshState :: (MonadIO m) => m State
+freshState = newTVarIO $ UserRepo.Store mempty
+
+instance UC.Logger App where
+  log = Logger.log
