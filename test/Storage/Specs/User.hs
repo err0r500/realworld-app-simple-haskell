f1b5488fd278d31630409e15de45133fa3e5b9ad
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2020-05-07 09:35:53 +0000

    clean up storage specs

--- test/Storage/Specs/User.hs
+++ test/Storage/Specs/User.hs
@@ -12,6 +12,14 @@ import qualified Storage.Lib                   as Lib
 import qualified Domain.User                   as D
 import qualified Usecase.Interactor            as UC
 
+appToIO :: Lib.Logs -> Lib.App a -> IO a
+appToIO logs x = liftIO $ Lib.run logs x
+
+resetAndGetLogs :: Lib.ResetFunc Lib.App -> IO Lib.Logs
+resetAndGetLogs reset = do
+  logs <- Lib.emptyLogs
+  appToIO logs $ reset ()
+  return logs
 
 spec :: UC.UserRepo Lib.App -> Lib.ResetFunc Lib.App -> Spec
 spec r reset = do
@@ -20,81 +28,60 @@ spec r reset = do
       otherUser = D.User fakeUUID2 "other" "other@example.com"
 
   -- Get User
-  describe "find user by ID" $ it "succeeds" $ do
-    logs         <- Lib.emptyLogs
-    Right result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      Nothing <- UC._insertUserPswd r otherUser ""
-      UC._getUserByID r uid
-    result `shouldBe` Just user
+  before (resetAndGetLogs reset) $ do
+    describe "find user by ID" $ it "succeeds" $ \logs -> do
+      Right result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        Nothing <- UC._insertUserPswd r otherUser ""
+        UC._getUserByID r uid
+      result `shouldBe` Just user
 
-  describe "find user by email" $ it "succeeds" $ do
-    logs         <- Lib.emptyLogs
-    Right result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      Nothing <- UC._insertUserPswd r otherUser ""
-      UC._getUserByEmail r (D._email user)
-    result `shouldBe` Just user
+    describe "find user by email" $ it "succeeds" $ \logs -> do
+      Right result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        Nothing <- UC._insertUserPswd r otherUser ""
+        UC._getUserByEmail r (D._email user)
+      result `shouldBe` Just user
 
-  describe "find user by name" $ it "succeeds" $ do
-    logs         <- Lib.emptyLogs
-    Right result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      Nothing <- UC._insertUserPswd r otherUser ""
-      UC._getUserByName r (D._name user)
-    result `shouldBe` Just user
+    describe "find user by name" $ it "succeeds" $ \logs -> do
+      Right result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        Nothing <- UC._insertUserPswd r otherUser ""
+        UC._getUserByName r (D._name user)
+      result `shouldBe` Just user
 
 
   -- Insert User
+  -- flaky test ! it depends on a schema detail...
+    describe "invalid user (empty email)" $ it "fails" $ \logs -> do
+      result <- appToIO logs $ UC._insertUserPswd r (otherUser { D._email = "" }) ""
+      result `shouldBe` Just UC.AnyErr
 
--- this test is dependant of a schema detail...
-  describe "invalid user (empty email)" $ it "fails" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      UC._insertUserPswd r (otherUser { D._email = "" }) ""
-    result `shouldBe` Just UC.AnyErr
-
-  describe "invalid user (empty name)" $ it "fails" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      UC._insertUserPswd r (otherUser { D._name = "" }) ""
-    result `shouldBe` Just UC.AnyErr
+    describe "invalid user (empty name)" $ it "fails" $ \logs -> do
+      result <- appToIO logs $ UC._insertUserPswd r (otherUser { D._name = "" }) ""
+      result `shouldBe` Just UC.AnyErr
 
 
-  describe "2 different users" $ it "succeeds" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      UC._insertUserPswd r otherUser ""
-    result `shouldBe` Nothing
+    describe "2 different users" $ it "succeeds" $ \logs -> do
+      result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        UC._insertUserPswd r otherUser ""
+      result `shouldBe` Nothing
 
+    describe "2 users with same id" $ it "fails" $ \logs -> do
+      result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        UC._insertUserPswd r (otherUser { D._id = uid }) ""
+      result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
 
-  describe "2 users with same id" $ it "fails" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      UC._insertUserPswd r (otherUser { D._id = uid }) ""
-    result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
+    describe "2 users with same name" $ it "fails" $ \logs -> do
+      result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        UC._insertUserPswd r (otherUser { D._name = D._name user }) ""
+      result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
 
-  describe "2 users with same name" $ it "fails" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      UC._insertUserPswd r (otherUser { D._name = D._name user }) ""
-    result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
-
-  describe "2 users with same email" $ it "fails" $ do
-    logs   <- Lib.emptyLogs
-    result <- liftIO $ Lib.run logs $ do
-      reset ()
-      Nothing <- UC._insertUserPswd r user ""
-      UC._insertUserPswd r (otherUser { D._email = D._email user }) ""
-    result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
+    describe "2 users with same email" $ it "fails" $ \logs -> do
+      result <- appToIO logs $ do
+        Nothing <- UC._insertUserPswd r user ""
+        UC._insertUserPswd r (otherUser { D._email = D._email user }) ""
+      result `shouldBe` Just (UC.SpecificErr UC.InsertUserConflict)
